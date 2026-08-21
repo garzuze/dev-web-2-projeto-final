dcc6a846d0d757dfd4620697379318081e719c43
garzuze <lucasgarzuze@gmail.com>
2026-08-21 16:34:45 -0300

    fix: import Routes type in app.route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { SolicitacaoServicoCliente } from './solicitacao-servico-cliente/solicitacao-servico-cliente';
 import { LoginComponent } from './login/login.component';
 import { QuoteComponent } from './budget/quote';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: 'solicitacao-servico-cliente', component: SolicitacaoServicoCliente },
