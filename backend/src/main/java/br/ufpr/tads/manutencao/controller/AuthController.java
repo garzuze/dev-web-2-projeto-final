feab48d47c4d06f392e95b75f5c551552ab0a338
Gabriel Albuquerque <albuquerque.gabrielc@gmail.com>
2026-08-27 17:58:36 -0300

    feat(auth): sign up and login endpoints

---
+++ backend/src/main/java/br/ufpr/tads/manutencao/controller/AuthController.java
@@ -0,0 +1,41 @@
+package br.ufpr.tads.manutencao.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.ufpr.tads.manutencao.dto.LoginRequest;
+import br.ufpr.tads.manutencao.dto.LoginResponse;
+import br.ufpr.tads.manutencao.dto.SignUpRequest;
+import br.ufpr.tads.manutencao.dto.SignUpResponse;
+import br.ufpr.tads.manutencao.service.LoginService;
+import br.ufpr.tads.manutencao.service.SignUpService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+  private final SignUpService signUpService;
+  private final LoginService loginService;
+
+  public AuthController(SignUpService signUpService, LoginService loginService) {
+    this.signUpService = signUpService;
+    this.loginService = loginService;
+  }
+
+  @PostMapping("/sign-up")
+  @ResponseStatus(HttpStatus.CREATED)
+  public SignUpResponse signUp(@Valid @RequestBody SignUpRequest request) {
+    return signUpService.signUp(request);
+  }
+
+  @PostMapping("/login")
+  public LoginResponse login(@Valid @RequestBody LoginRequest request) {
+    return loginService.login(request);
+  }
+
+}
