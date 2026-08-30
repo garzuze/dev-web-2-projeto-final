7f541e95590a392b80f7a2a08e04b20e0d4e2d3e
vitor-felipee <vitor07.felipe@gmail.com>
2026-08-30 00:30:47 -0300

    feat: implement QuoteComponent to display maintenance request details

---
+++ frontend/src/app/pages/client/quote/quote.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { MaintenanceRequestService } from '../../../services/maintenance-request.service';
+import { MaintenanceRequest } from '../../../models/maintenanceRequest.model';
+import { CurrencyPipe, DatePipe, LowerCasePipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  imports: [CurrencyPipe, DatePipe, LowerCasePipe],
+  selector: 'app-quote',
+  styleUrl: './quote.component.scss',
+  templateUrl: './quote.component.html',
+})
+export class QuoteComponent {
+  private maintenanceRequestService = inject(MaintenanceRequestService);
+  private activatedRoute = inject(ActivatedRoute);
+  public requestData?: MaintenanceRequest;
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.maintenanceRequestService.getMaintenanceRequestById(id).subscribe((request) => {
+        this.requestData = request;
+      });
+    });
+  }
+}
