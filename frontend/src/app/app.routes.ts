a35becc32dd37c2e0647057b74b767b196c095c9
vitor-felipee <vitor07.felipe@gmail.com>
2026-08-29 23:36:17 -0300

    refactor: updated dynamic route for the quote component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { SolicitacaoServicoCliente } from './solicitacao-servico-cliente/solicitacao-servico-cliente';
 import { LoginComponent } from './login/login.component';
-import { QuoteComponent } from './budget/quote';
+import { QuoteComponent } from './pages/client/quote/';
 import { PaymentComponent } from './payment/payment';
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
@@ -10,7 +10,7 @@ export const routes: Routes = [
   { path: 'solicitacao-servico-cliente', component: SolicitacaoServicoCliente },
   { path: 'login', component: LoginComponent },
   { path: '', component: LandingPage },
-  { path: 'quote', component: QuoteComponent },
+  { path: 'quote/:id', component: QuoteComponent },
   { path: 'payment', component: PaymentComponent },
   { path: 'autocadastro', component: Autocadastro },
 ];
