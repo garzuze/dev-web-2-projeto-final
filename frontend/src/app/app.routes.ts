c6c566b293d282f89cec512131435aa87b86b887
vitor-felipee <vitor07.felipe@gmail.com>
2026-09-02 20:11:46 -0300

    feat: update payment component import path and add index file for exports

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { SolicitacaoServicoCliente } from './solicitacao-servico-cliente/solicitacao-servico-cliente';
 import { LoginComponent } from './login/login.component';
 import { QuoteComponent } from './pages/client/quote/';
-import { PaymentComponent } from './payment/payment';
+import { PaymentComponent } from './pages/client/payment';
 import { Routes } from '@angular/router';
 import { LandingPage } from './landing-page/landing-page';
 import { Autocadastro } from './autocadastro/autocadastro';
