a78391a4d8ab7abdd6d8e31479af60fd80e92153
garzuze <lucasgarzuze@gmail.com>
2026-09-10 16:07:35 -0300

    feat: add header component to landing page screen

--- frontend/src/app/landing-page/landing-page.ts
+++ frontend/src/app/landing-page/landing-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Header } from '../components/header/header';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, Header],
   selector: 'app-landing-page',
   styleUrl: './landing-page.scss',
   templateUrl: './landing-page.html',
