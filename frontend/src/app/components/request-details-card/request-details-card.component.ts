21dac299db5acdb449803f2a3be4bf0b18f4da6c
vitor-felipee <vitor07.felipe@gmail.com>
2026-09-11 00:26:45 -0300

    feat: add history section to request details card component

--- frontend/src/app/components/request-details-card/request-details-card.component.ts
+++ frontend/src/app/components/request-details-card/request-details-card.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { MaintenanceRequest, RequestStatus } from '../../models/maintenanceRequest.model';
-import { CurrencyPipe, DatePipe, LowerCasePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, LowerCasePipe, NgClass } from '@angular/common';
 
 @Component({
-  imports: [DatePipe, LowerCasePipe, CurrencyPipe],
+  imports: [DatePipe, LowerCasePipe, CurrencyPipe, NgClass],
   selector: 'app-request-details-card',
   styleUrl: './request-details-card.component.scss',
   templateUrl: './request-details-card.component.html',
