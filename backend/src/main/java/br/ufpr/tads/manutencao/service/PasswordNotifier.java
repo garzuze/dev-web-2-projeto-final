523f3090514f92e3daa5f4381fcb0583b92b272e
Yohan Cys <yohanbcys@gmail.com>
2026-09-13 00:35:37 -0300

    feat: implementa serviço de envio de email com a senha gerada durante o autocadastro

--- backend/src/main/java/br/ufpr/tads/manutencao/service/PasswordNotifier.java
+++ backend/src/main/java/br/ufpr/tads/manutencao/service/PasswordNotifier.java
@@ -9,8 +9,33 @@ public class PasswordNotifier {
 
     private static final Logger log = LoggerFactory.getLogger(PasswordNotifier.class);
 
+    private final EmailService servicoEmail;
+
+    public PasswordNotifier(EmailService servicoEmail){
+        this.servicoEmail = servicoEmail;
+    }
+
     public void send(String email, String password) {
-        log.info("Generated password for {}: {}", email, password);
+        try {
+            log.info("Generated password for {}: {}", email, password);
+            String assunto = "Senha de Acesso - Manutenção Zica 😎";    
+            String corpo = """
+                    Olá!
+                    
+                    Muito obrigado por se cadastrar no sistema de manutenção mais zica do Brasil 😎 !!!
+
+                    Sua senha de acesso é: %s
+
+                    Utilize a senha fornecida acima junto com seu email para realizar login no sistema.
+                    """.formatted(password);
+
+                    servicoEmail.enviaEmail(email, assunto, corpo);
+                    log.info("E-mail com senha enviado para: {}", email);
+        } catch (Exception e) {
+            log.error("Falha no envio de email para {}: {}", email, e.getMessage());
+            log.info("Generated password for {}: {}", email, password);
+        }
+        
     }
 
 }
